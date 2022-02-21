65ec7ea9802cafab225e7aa73717e56e1fe0e3af
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-02-21 11:35:32 -0800

    Day 8 Added

---
+++ Day 1/README.md
@@ -0,0 +1,11 @@
+# 100DaysOfCode 💻
+
+Day 1 of code challenge 😍
+
+# Overview
+
+Youtube Downloader Python Gui Based Video + Audio Downloader
+
+# Requirement
+
+pip install pytube
--- Day 4/django_todo_app/base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-#Using function base view
-def taskList(request):
-    return HttpResponse('To-Do Lists')
-
